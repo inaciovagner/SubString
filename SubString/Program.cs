7e9f4833bcf63ff598c3fb459eefd9d760d47126
Vagner de Souza <inaciovagner1@gmail.com>
2021-04-06 13:10:55 +0100

    Add project files.

---
+++ SubString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              
+            string str = "Hi Ucertify!!!";
+
+               
+            string Hi = str.Substring(3, 8);
+            Console.WriteLine(Hi);
+
+             
+            string Ucertify = str.Substring(3);
+            Console.WriteLine(Ucertify);
+
+            Console.ReadKey();
+        }
+    }
+}
